9a0dc68cbeabb5331f26fbebce49fa8fb3954584
ImeneeSh <nourelimene.sahi@se.univ-bejaia.dz>
2025-07-08 11:51:43 +0100

    test de la connexion avec la bdd

--- src/main/java/org/example/corthreads/CorthreadsApplication.java
+++ src/main/java/org/example/corthreads/CorthreadsApplication.java
@@ -3,11 +3,24 @@ package org.example.corthreads;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired ;
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class CorthreadsApplication {
+public class CorthreadsApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
 
     public static void main(String[] args) {
+
         SpringApplication.run(CorthreadsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("connexion reussi !");
     }
 
 }
