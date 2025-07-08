512b0c4008aab431043853e01b4b5777589d9e12
ImeneeSh <nourelimene.sahi@se.univ-bejaia.dz>
2025-07-08 14:25:23 +0100

    création de l'entité utilisateur

---
+++ src/main/java/org/example/corthreads/models/Utilisateur.java
@@ -0,0 +1,74 @@
+package org.example.corthreads.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity // indique que cette classe est une table en bdd
+@Data //genère les getters + setters automatiquement
+@Table (name ="utilisateur")
+public class Utilisateur {
+
+    @Id // pour indiquer que c'est une clé primaire
+    @Column(name = "idUser")
+    private String idUser ;
+
+    @Column(nullable = false ) // champ obligatoire
+    private String nom ;
+
+    @Column(nullable = false )
+    private String prenom ;
+
+    @Column(nullable = false )
+    private String wilaya ;
+
+    @Column(nullable = false)
+    private LocalDate dateNaissance ; //LocalDate correspond au DATE dans sql
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role ;
+
+    @Column(nullable = false)
+    private String mdp ; // il sera haché
+
+    @Enumerated(EnumType.STRING)
+    private GroupeSang groupeSang;
+
+    private String rh;
+
+    private String specialite ;
+
+    private String etablissement ;
+
+    @Column(name = "estDonneurOrgane") // on fait ça quand le nom dans la bdd diffère de celui en java
+    private boolean donneurOrgane ;
+
+    @Column(name = "estDonneurFoie")
+    private boolean donneurFoie ;
+
+    @Column(name = "estDonneurSang")
+    private boolean donneurSang ;
+
+    @Column(name = "dateValidationDon")
+    private LocalDate dateValidationDon;
+
+    @Column(name = "dernierDonSang")
+    private LocalDate dernierDonSang;
+
+    @Column(name = "aDonneFoie")
+    private boolean aDonneFoie;
+
+}
+enum Role {
+    Citoyen,
+    Medecin,
+    Admin
+}
+
+enum GroupeSang {
+    A, B, AB, O
+}
+
+
