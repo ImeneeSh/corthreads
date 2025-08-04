27fe0e58537e9b685b670349ed0c90965f9b16d5
ImeneeSh <nourelimene.sahi@se.univ-bejaia.dz>
2025-08-04 16:52:44 +0100

    suppression de la dev tools en mode production

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import AOS from 'aos'
 import './main.css'
 
+if (import.meta.env.PROD) {
+    window.__VUE_DEVTOOLS_GLOBAL_HOOK__ = undefined;
+}
+
 createApp(App)
     .use(router)
     .mount('#app')
